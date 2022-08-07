14a761dabb496a2ef740995a6a55b855b0a9c2c4
BENJA_303 <66800616+BiberGames@users.noreply.github.com>
2022-08-07 21:17:14 +0200

    Some discord fixes.

--- sl-client/Assets/Scripts/Discord/DiscordManager.cs
+++ sl-client/Assets/Scripts/Discord/DiscordManager.cs
@@ -8,44 +8,53 @@ public class DiscordManager : MonoBehaviour
 
     void Awake()
     {
-        try
+        if (!Application.isEditor)
         {
-            _Discord = new Discord.Discord(1005901649698619493, (System.UInt64)Discord.CreateFlags.Default);
-            var ActivityManager = _Discord.GetActivityManager();
-            var Activity = new Discord.Activity {
-                Details = "Wasting my time.",
-                State = "alone.",
-                Assets = {
-                    LargeImage = "logo_4x",
-                }
-            };
-            ActivityManager.UpdateActivity(Activity, (res) => {
-                if(res == Discord.Result.Ok)
-                {
-                    GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connected to Discord!<color=#FFFFFF>");
-                }
-                else
-                {
-                    GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connection to Discord failed :(<color=#FFFFFF>");
-                }
-            });
+            try
+            {
+                _Discord = new Discord.Discord(1005901649698619493, (System.UInt64)Discord.CreateFlags.Default);
+                var ActivityManager = _Discord.GetActivityManager();
+                var Activity = new Discord.Activity {
+                    Details = "Wasting my time.",
+                    State = "alone.",
+                    Assets = {
+                        LargeImage = "logo_4x",
+                    }
+                };
+                ActivityManager.UpdateActivity(Activity, (res) => {
+                    if(res == Discord.Result.Ok)
+                    {
+                        GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connected to Discord!<color=#FFFFFF>");
+                    }
+                    else
+                    {
+                        GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connection to Discord failed :(<color=#FFFFFF>");
+                    }
+                });
+            }
+            catch (Exception e)
+            {
+                GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connection to Discord failed :(<color=#FFFFFF>");
+            }
         }
-        catch (Exception e)
+        else
         {
-            GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Connection to Discord failed :(<color=#FFFFFF>");
+            GameObject.Find("Console").GetComponent<Console>().AddLine("\n<color=#7289da>Discord > Running in editor not connecting.<color=#FFFFFF>");
         }
-
     }
 
     void Update()
     {
-        try
+        if (!Application.isEditor)
         {
-            _Discord.RunCallbacks();
-        }
-        catch (Exception e)
-        {
-            Debug.Log("");
+            try
+            {
+                _Discord.RunCallbacks();
+            }
+            catch (Exception e)
+            {
+                Debug.Log("");
+            }
         }
     }
 }
