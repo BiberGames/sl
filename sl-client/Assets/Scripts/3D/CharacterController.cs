c7e47a26427d23a3133160eebc93268cf929b3a8
BENJA_303 <66800616+BiberGames@users.noreply.github.com>
2022-10-23 02:12:47 +0200

    Added Nocilp and state machine to fps controller

--- sl-client/Assets/Scripts/3D/CharacterController.cs
+++ sl-client/Assets/Scripts/3D/CharacterController.cs
@@ -22,24 +22,36 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private float InAirMultiplier = 0.45f;
     [SerializeField]
+    private float NoclipSpeed = 5f;
+    [SerializeField]
     private float JumpForce = 5f;
     [SerializeField]
     private float GravityOnGround;
     private float MoveMul = 1;
+    private Vector2 Axis;
     [SerializeField]
     private bool Grounded = false;
     [SerializeField]
     private LayerMask _LayerMask;
     [SerializeField]
     private Transform GroundCheck;
-
-    float MoveX;
-    float MoveZ;
+    [SerializeField]
+    private State CharacterControllerState;
 
     [SerializeField]
     private Transform PlayerBody;
     [SerializeField]
     private Rigidbody PlayerRigidbody;
+
+    public enum State
+    {
+        Crouching,
+        Walking,
+        Sprinting,
+        Swimming,
+        Climmbing,
+        Noclip
+    }
 
     private void Start()
     {
@@ -49,39 +61,74 @@ public class CharacterController : MonoBehaviour
 
         PlayerRigidbody = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        CharacterControllerState = State.Walking;
     }
 
     // Update is called once per frame
     private void Update()
     {
         Grounded = Physics.CheckSphere(GroundCheck.position, 0.4f, _LayerMask);
-
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
-            PlayerRigidbody.velocity = new Vector3(PlayerRigidbody.velocity.x, JumpForce, PlayerRigidbody.velocity.z);
-
-        if(Grounded)
+        Axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * MoveSpeed;
+        
+        if (Grounded)
+        {
             MoveMul = 1f;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+                PlayerRigidbody.velocity = new Vector3(PlayerRigidbody.velocity.x, JumpForce, PlayerRigidbody.velocity.z);
+        }
         else
+        {
             MoveMul = InAirMultiplier;
+        }
 
         MouseLook();
     }
 
     void FixedUpdate ()
     {
-        MovePlayer();
+        switch(CharacterControllerState)
+        {
+            case State.Crouching:
+                NormalMovement();
+                break;
+
+            case State.Walking:
+                NormalMovement();
+                break;
+
+            case State.Sprinting:
+                NormalMovement();
+                break;
+
+            case State.Noclip:
+                NoclipMovement();
+                break;
+        }
     }
 
-    private void MovePlayer()
+    private void NormalMovement()
     {
-        Vector2 Axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * MoveSpeed;
+        PlayerRigidbody.useGravity = true;
+        PlayerBody.GetComponent<CapsuleCollider>().enabled = true;
         Axis = Axis * MoveMul;
 
-        Vector3 Forward = new Vector3(-Camera.main.transform.right.z, 0.0f, Camera.main.transform.right.x);
+        Vector3 Forward = new Vector3(-CameraPivot.transform.right.z, 0.0f, CameraPivot.transform.right.x);
 
-        Vector3 Direction = (Forward * Axis.x + Camera.main.transform.right * Axis.y + Vector3.up * PlayerRigidbody.velocity.y);
+        Vector3 Direction = (Forward * Axis.x + CameraPivot.transform.right * Axis.y + Vector3.up * PlayerRigidbody.velocity.y);
 
         PlayerRigidbody.velocity = Direction;
+    }
+
+    private void NoclipMovement()
+    {
+        PlayerRigidbody.useGravity = false;
+        PlayerBody.GetComponent<CapsuleCollider>().enabled = false;
+
+        Vector3 Direction = (CameraPivot.forward * Axis.x + CameraPivot.right * Axis.y) * (NoclipSpeed / 100);
+
+        PlayerBody.position += Direction;//CameraPivot.forward + new Vector3(1,0,0) * Axis.y;
     }
 
     private void MouseLook()
